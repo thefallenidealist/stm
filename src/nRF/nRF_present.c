41fbaa0d37589bf204e75ac27448faa13e4c42a4
Johnny S <propaliidealist@gmail.com>
2015-06-09 14:12:38 +0000

    misc add

---
+++ src/nRF/nRF_present.c
@@ -0,0 +1,23 @@
+/*************************************************************************************************
+				nRF_is_present()
+*************************************************************************************************/
+bool nRF_is_present(nRF_hw_t *nRF0)
+{
+	uint8_t address1[] = {0x00, 0x01, 0x02, 0x03, 0x04};
+	uint8_t *address2 = NULL;
+	uint8_t address_width = nRF_get_address_width(nRF0);
+
+	nRF_set_TX_address(nRF0, address1);
+	delay_ms(1);
+	address2 = nRF_get_TX_address(nRF0);
+
+	for (uint8_t i=0; i<address_width; i++)
+	{
+		if (address1[i] != *address2++)
+		{
+			return 0;
+		}
+	}
+
+	return 1;
+}
